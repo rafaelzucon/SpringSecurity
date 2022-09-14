0f13b1af86c22935bae08ee6cd9ab2e64427a9cf
rafaelzucon <rafazucon@gmail.com>
2022-09-14 08:12:28 -0300

    Aplicando Basic Auth com customização em memória e encoder da anotação @Bean

--- src/main/java/com/api/pcj18sb273/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/pcj18sb273/configs/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.pcj18sb273.configs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter { //WebSecurityConfigurerAdapter is Deprecated in Java 18!!!
@@ -14,6 +18,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { //WebSecur
                 .and()
                 .authorizeHttpRequests()
                 //.anyRequest().permitAll(); // permitAll() permite todas as requisições, não é feita autenticação
-                .anyRequest().authenticated(); // com authenticated() as requisições são, obrigatoriamente, autenticadas
+                .anyRequest().authenticated() // com authenticated() as requisições são, obrigatoriamente, autenticadas
+                .and()
+                .csrf().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("asdrubal")
+                .password(passwordEncoder().encode("06081976"))
+                .roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
